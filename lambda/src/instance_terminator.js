7084db4864391f0287a91c5877b4794a6d34e5ff
Dave Shepherd <david.shepherd@wealthwizards.com>
2018-01-12 14:10:26 +0000

    List the intances that have been terminatd to response object

--- lambda/src/instance_terminator.js
+++ lambda/src/instance_terminator.js
@@ -8,25 +8,38 @@ exports.handler = ( event, context, callback ) => {
     var tagKey = 'can-be-terminated'
     var tagValue = 'true'
 
-    /*
-    TODO:
-    * allow parameters to be overridden
-    * collect results and return them/fail (use Promise.all(): https://stackoverflow.com/questions/38426745/how-do-i-return-the-accumulated-results-of-multiple-parallel-asynchronous-func )
-     */
-    const autoscaling = new AWS.AutoScaling();
-
-    autoscaling.describeAutoScalingGroups({}, function(err, data) {
-        if (err) {
-            console.log(err, err.stack);
-            return callback(null, {success: false});
-        } else {
-            const matchingAutoscalingGroups = data['AutoScalingGroups'].filter(item => containsTag(item['Tags'], tagKey, tagValue));
-            matchingAutoscalingGroups.forEach(item => terminateInstanceIn(item))
-        }
-    });
-
-    return callback( null, { success: true } );
+    getAutoscalingGroups(tagKey, tagValue)
+        .then(function(autoscalingGroups) {
+            terminateInstanceFromEachAutoscalingGroupIn(autoscalingGroups)
+                .then(function(results) {
+                    var result = {
+                        terminated_instances: results
+                    };
+                    callback( null, result );
+                }, function(reason) {
+                    console.log('Some async call failed:');
+                    console.log(' --> ', reason);
+                    callback(reason);
+                });
+        }, function(reason) {
+            console.log(reason, reason.stack);
+            callback(reason);
+        });
 };
+
+function getAutoscalingGroups(tagKey, tagValue) {
+    return new Promise(function(resolve, reject) {
+        const autoscaling = new AWS.AutoScaling();
+        autoscaling.describeAutoScalingGroups({}, function(err, data) {
+            if (err) {
+                reject(err);
+            } else {
+                const matchingAutoscalingGroups = data['AutoScalingGroups'].filter(item => containsTag(item['Tags'], tagKey, tagValue));
+                resolve(matchingAutoscalingGroups);
+            }
+        });
+    });
+}
 
 function containsTag(tags, tagKey, tagValue) {
     return tags.some(function (tag) {
@@ -35,26 +48,36 @@ function containsTag(tags, tagKey, tagValue) {
     })
 }
 
-function terminateInstanceIn(autoscalingGroup) {
+function terminateInstanceFromEachAutoscalingGroupIn(autoscalingGroups){
+    var promises = [];
+    autoscalingGroups.forEach(function(item) {
+        promises.push(new Promise(function(resolve, reject) {
+            terminateInstanceIn(item, resolve, reject);
+        }));
+    })
+    return Promise.all(promises);
+}
+
+function terminateInstanceIn(autoscalingGroup, resolve, reject) {
     console.log('Attempting to terminate instance from: ' + autoscalingGroup['AutoScalingGroupName']);
     var desiredCapacity = autoscalingGroup['DesiredCapacity'];
     console.log('Autoscaling group is expecting ' + desiredCapacity + ' instances');
     if (desiredCapacity < 2) {
         console.log('Too few instances, ignoring.');
-        return;
+        resolve('none');
     }
-    // Are all instances healthy and is there the right number?
-    //const result = words.filter(word => word.length > 6);
     var instances = autoscalingGroup['Instances'];
     const healthyInstances = instances.filter(instance => instance['LifecycleState'] == 'InService' && instance['HealthStatus'] == 'Healthy');
 
     if (healthyInstances.length < desiredCapacity) {
         console.log('Too few healthy instances, ignoring.')
+        resolve('none');
     } else {
 
         lookupOldestInstance(instances)
             .then(function (oldestInstance) {
                 terminateInstance(oldestInstance['InstanceId'])
+                resolve(oldestInstance['InstanceId']);
             }).catch(function (error) {
                 console.log(error);
             });
--- lambda/test/instance_terminator.test.js
+++ lambda/test/instance_terminator.test.js
@@ -27,7 +27,7 @@ describe( 'instance-terminator', function() {
         return LambdaTester( myLambda.handler )
             .event()
             .expectResult( ( result ) => {
-                expect( result.success ).to.be.true;
+                expect( result.terminated_instances).to.have.lengthOf( 0 );
                 expect( describeInstances.notCalled, 'describe instances should not be called' ).to.be.true;
                 expect( terminateInstance.notCalled, 'terminate instance should not be called' ).to.be.true;
             });
@@ -45,7 +45,8 @@ describe( 'instance-terminator', function() {
         return LambdaTester( myLambda.handler )
             .event()
             .expectResult( ( result ) => {
-                expect( result.success ).to.be.true;
+                expect( result.terminated_instances).to.have.lengthOf( 1 );
+                expect( result.terminated_instances[0] ).to.equal( 'none' );
                 expect( describeInstances.notCalled, 'describe instances should not be called' ).to.be.true;
                 expect( terminateInstance.notCalled, 'terminate instance should not be called' ).to.be.true;
         });
@@ -63,7 +64,8 @@ describe( 'instance-terminator', function() {
         return LambdaTester( myLambda.handler )
             .event()
             .expectResult( ( result ) => {
-                expect( result.success ).to.be.true;
+                expect( result.terminated_instances).to.have.lengthOf( 1 );
+                expect( result.terminated_instances[0] ).to.equal( 'none' );
                 expect( describeInstances.notCalled, 'describe instances should not be called' ).to.be.true;
                 expect( terminateInstance.notCalled, 'terminate instance should not be called' ).to.be.true;
         });
@@ -83,14 +85,50 @@ describe( 'instance-terminator', function() {
         return LambdaTester( myLambda.handler )
             .event()
             .expectResult( ( result ) => {
-                expect( result.success ).to.be.true;
+                expect( result.terminated_instances).to.have.lengthOf( 1 );
+                expect( result.terminated_instances[0] ).to.equal( 'i-instance-2' );
                 expect( terminateInstance.calledOnce, 'terminate instance called once' ).to.be.true;
 
                 var expectedParams = {
-                    InstanceId: 'i-087792a26bd5f156b',
+                    InstanceId: 'i-instance-2',
                     ShouldDecrementDesiredCapacity: false
                 };
                 expect( terminateInstance.calledWith(expectedParams), 'terminate instance parameters' ).to.be.true;
+            });
+    });
+
+    it( `successful invocation_with_multiple_asgs`, function() {
+
+        AWS.mock('AutoScaling', 'describeAutoScalingGroups', function (params, callback){
+            callback(null, describeAutoscalingGroupsResponse_multiple_asgs);
+        });
+        AWS.mock('EC2', 'describeInstances', function (params, callback){
+            if (params['InstanceIds'].indexOf('i-multiple-asgs-1') > -1) {
+                callback(null, describeInstances_with_multiple_asgs_1);
+            } else {
+                callback(null, describeInstances_with_multiple_asgs_2);
+            }
+        });
+        var terminateInstance = sinon.spy();
+        AWS.mock('AutoScaling', 'terminateInstanceInAutoScalingGroup', terminateInstance);
+
+        return LambdaTester( myLambda.handler )
+            .event()
+            .expectResult( ( result ) => {
+                expect( result.terminated_instances).to.have.lengthOf( 2 );
+                expect( result.terminated_instances).to.have.members(['i-multiple-asgs-2', 'i-multiple-asgs-4']);
+                expect( terminateInstance.callCount, 'terminate instance called twice' ).to.equal(2);
+
+                var expectedParams1 = {
+                    InstanceId: 'i-multiple-asgs-2',
+                    ShouldDecrementDesiredCapacity: false
+                };
+                expect( terminateInstance.calledWith(expectedParams1), 'terminate instance parameters' ).to.be.true;
+                var expectedParams2 = {
+                    InstanceId: 'i-multiple-asgs-4',
+                    ShouldDecrementDesiredCapacity: false
+                };
+                expect( terminateInstance.calledWith(expectedParams2), 'terminate instance parameters' ).to.be.true;
             });
     });
 });
@@ -180,14 +218,14 @@ var describeAutoscalingGroupsResponse_unhealthy_instance = {
             MaxSize: 2,
             DesiredCapacity: 2,
             Instances:  [{
-                InstanceId: 'i-03a2a8ad7e70a48b3',
+                InstanceId: 'i-unhealth-instance-1',
                 AvailabilityZone: 'eu-west-1c',
                 LifecycleState: 'Pending',
                 HealthStatus: 'Healthy',
                 LaunchConfigurationName: 'my-asg-unhealthy-instance-20180108195930693600000001',
                 ProtectedFromScaleIn: false
             }, {
-                InstanceId: 'i-087792a26bd5f156b',
+                InstanceId: 'i-unhealth-instance-2',
                 AvailabilityZone: 'eu-west-1b',
                 LifecycleState: 'InService',
                 HealthStatus: 'Healthy',
@@ -235,14 +273,14 @@ var describeAutoscalingGroupsResponse = {
             MaxSize: 2,
             DesiredCapacity: 2,
             Instances:  [{
-                InstanceId: 'i-03a2a8ad7e70a48b3',
+                InstanceId: 'i-instance-1',
                 AvailabilityZone: 'eu-west-1c',
                 LifecycleState: 'InService',
                 HealthStatus: 'Healthy',
                 LaunchConfigurationName: 'my-asg-20180108195930693600000001',
                 ProtectedFromScaleIn: false
             }, {
-                InstanceId: 'i-087792a26bd5f156b',
+                InstanceId: 'i-instance-2',
                 AvailabilityZone: 'eu-west-1b',
                 LifecycleState: 'InService',
                 HealthStatus: 'Healthy',
@@ -279,11 +317,87 @@ var describeAutoscalingGroupsResponse = {
     ]
 }
 
+var describeAutoscalingGroupsResponse_multiple_asgs = {
+    ResponseMetadata: {
+        RequestId: 'd3f3e7ea-f6b5-11e7-b320-07888c59bfce'
+    },
+    AutoScalingGroups: [
+        {
+            AutoScalingGroupName: 'my-asg-multiple-asgs',
+            MinSize: 2,
+            MaxSize: 2,
+            DesiredCapacity: 2,
+            Instances:  [{
+                InstanceId: 'i-multiple-asgs-1',
+                AvailabilityZone: 'eu-west-1c',
+                LifecycleState: 'InService',
+                HealthStatus: 'Healthy',
+                LaunchConfigurationName: 'my-asg-multiple-asgs-20180108195930693600000001',
+                ProtectedFromScaleIn: false
+            }, {
+                InstanceId: 'i-multiple-asgs-2',
+                AvailabilityZone: 'eu-west-1b',
+                LifecycleState: 'InService',
+                HealthStatus: 'Healthy',
+                LaunchConfigurationName: 'my-asg-multiple-asgs-20180108195930693600000001',
+                ProtectedFromScaleIn: false
+            }],
+            Tags: [{
+                ResourceId: 'my-asg-multiple-asgs',
+                ResourceType: 'auto-scaling-group',
+                Key: 'Name',
+                Value: 'my-asg-multiple-asgs',
+                PropagateAtLaunch: true
+            }, {
+                ResourceId: 'my-asg-multiple-asgs',
+                ResourceType: 'auto-scaling-group',
+                Key: 'can-be-terminated',
+                Value: 'true',
+                PropagateAtLaunch: true
+            }]
+        },
+        {
+            AutoScalingGroupName: 'another-asg-multiple-asgs',
+            MinSize: 2,
+            MaxSize: 2,
+            DesiredCapacity: 2,
+            Instances:  [{
+                InstanceId: 'i-multiple-asgs-3',
+                AvailabilityZone: 'eu-west-1c',
+                LifecycleState: 'InService',
+                HealthStatus: 'Healthy',
+                LaunchConfigurationName: 'another-asg-multiple-asgs-20180108195930693600000001',
+                ProtectedFromScaleIn: false
+            }, {
+                InstanceId: 'i-multiple-asgs-4',
+                AvailabilityZone: 'eu-west-1b',
+                LifecycleState: 'InService',
+                HealthStatus: 'Healthy',
+                LaunchConfigurationName: 'another-asg-multiple-asgs-20180108195930693600000001',
+                ProtectedFromScaleIn: false
+            }],
+            Tags: [{
+                ResourceId: 'another-asg-multiple-asgs',
+                ResourceType: 'auto-scaling-group',
+                Key: 'Name',
+                Value: 'another-asg-multiple-asgs',
+                PropagateAtLaunch: true
+            }, {
+                ResourceId: 'another-asg-multiple-asgs',
+                ResourceType: 'auto-scaling-group',
+                Key: 'can-be-terminated',
+                Value: 'true',
+                PropagateAtLaunch: true
+            }]
+        }
+    ]
+}
+
 var describeInstances = {
     Reservations: [{
         Groups: [],
         Instances: [{
-            InstanceId: 'i-03a2a8ad7e70a48b3',
+            InstanceId: 'i-instance-1',
             LaunchTime: new Date('2018-01-11T11:55:22.000Z'),
             PrivateDnsName: 'ip-10-100-130-46.eu-west-1.compute.internal',
             PrivateIpAddress: '10.100.130.46',
@@ -298,7 +412,75 @@ var describeInstances = {
     }, {
         Groups: [],
         Instances: [{
-            InstanceId: 'i-087792a26bd5f156b',
+            InstanceId: 'i-instance-2',
+            LaunchTime: new Date('2018-01-11T09:56:50.000Z'),
+            PrivateDnsName: 'ip-10-100-129-65.eu-west-1.compute.internal',
+            PrivateIpAddress: '10.100.129.65',
+            State: {
+                Code: 16,
+                Name: 'running'
+            },
+        }],
+        OwnerId: '123456789012',
+        RequesterId: '123456789012',
+        ReservationId: 'r-021e47583509a7a3e'
+    }]
+}
+
+var describeInstances_with_multiple_asgs_1 = {
+    Reservations: [{
+        Groups: [],
+        Instances: [{
+            InstanceId: 'i-multiple-asgs-1',
+            LaunchTime: new Date('2018-01-11T11:55:22.000Z'),
+            PrivateDnsName: 'ip-10-100-130-46.eu-west-1.compute.internal',
+            PrivateIpAddress: '10.100.130.46',
+            State: {
+                Code: 16,
+                Name: 'running'
+            }
+        }],
+        OwnerId: '123456789012',
+        RequesterId: '123456789012',
+        ReservationId: 'r-0aff744b37d85b3dc'
+    }, {
+        Groups: [],
+        Instances: [{
+            InstanceId: 'i-multiple-asgs-2',
+            LaunchTime: new Date('2018-01-11T09:56:50.000Z'),
+            PrivateDnsName: 'ip-10-100-129-65.eu-west-1.compute.internal',
+            PrivateIpAddress: '10.100.129.65',
+            State: {
+                Code: 16,
+                Name: 'running'
+            },
+        }],
+        OwnerId: '123456789012',
+        RequesterId: '123456789012',
+        ReservationId: 'r-021e47583509a7a3e'
+    }]
+}
+
+var describeInstances_with_multiple_asgs_2 = {
+    Reservations: [{
+        Groups: [],
+        Instances: [{
+            InstanceId: 'i-multiple-asgs-3',
+            LaunchTime: new Date('2018-01-11T11:55:22.000Z'),
+            PrivateDnsName: 'ip-10-100-130-46.eu-west-1.compute.internal',
+            PrivateIpAddress: '10.100.130.46',
+            State: {
+                Code: 16,
+                Name: 'running'
+            }
+        }],
+        OwnerId: '123456789012',
+        RequesterId: '123456789012',
+        ReservationId: 'r-0aff744b37d85b3dc'
+    }, {
+        Groups: [],
+        Instances: [{
+            InstanceId: 'i-multiple-asgs-4',
             LaunchTime: new Date('2018-01-11T09:56:50.000Z'),
             PrivateDnsName: 'ip-10-100-129-65.eu-west-1.compute.internal',
             PrivateIpAddress: '10.100.129.65',
